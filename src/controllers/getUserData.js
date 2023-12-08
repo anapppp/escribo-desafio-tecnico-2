00538f782eaaffbd743311b08423c4e4b92e8096
anapppp <anapaula.ksoares@gmail.com>
2023-12-07 22:39:58 -0300

    update getUserData

--- src/controllers/getUserData.js
+++ src/controllers/getUserData.js
@@ -1,9 +1,15 @@
+const knex = require('../conection');
+
 const getUserData = async (req, res) => {
     try {
+        const telefones = await knex(
+            'telefone').select('ddd', 'numero').where({ user_id: req.user.id });
+
         return res.status(200).json({
             "id": req.user.id,
             "nome": req.user.nome,
-            "email": req.user.email
+            "email": req.user.email,
+            telefones
         });
     } catch (error) {
         return res.status(500).json({ "mensagem": "mensagem de erro" });
